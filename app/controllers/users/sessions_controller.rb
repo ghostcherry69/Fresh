b9837f3dd57ac4636ff5e8d289c889115bc04e4a
unknown <ghostcherry69@gmail.com>
2020-11-06 00:15:12 +0900

    admin,user,anglerのdevise機能実装

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,9 +9,13 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    if admin_signed_in?
+      flash[:notice] = "管理者アカウントをログアウトして下さい。"
+      redirect_to admin_top_path
+    end
+    super
+  end
 
   # DELETE /resource/sign_out
   # def destroy
@@ -19,6 +23,13 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # protected
+
+  def after_sign_in_path_for(resource)
+      user_plofile_path(current_user)
+  end
+  def after_sign_out_path_for(resource)
+      new_user_session_path
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
